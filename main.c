9f33d7c899cffae741bc7d46c8a095bf40b8ea4a
KMG <it26100418@my.sliit.lk>
2026-09-07 18:57:25 +0530

    Main

--- main.c
+++ main.c
@@ -3,47 +3,24 @@
 #include <time.h>
 
 #include "setup.h"
-#include "part1A.h"
-#include "part1B.h"
-#include "part1C.h"
+#include "menu.h"
 
 
 int main(void)
 {
     Battlefield field;
-    Battlefield part1BField;
-    Battlefield part1CField;
 
+    /* Start random number generator */
     srand((unsigned int)time(NULL));
 
-    /*
-     * Create initial battlefield.
-     */
+    /* Create the initial battlefield */
     initializeBattlefield(&field);
 
+    /* Show initial battlefield */
     displayBattlefield(&field);
 
-    /*
-     * Keep original battlefield
-     * for each part.
-     */
-    part1BField = field;
-    part1CField = field;
-
-    /*
-     * Part 1-A
-     */
-    runPart1A(&field);
-
-    /*
-     * Part 1-B
-     */
-    runPart1B(&part1BField);
-
-    /*
-     * Part 1-C
-     */
-    runPart1C(&part1CField);
+    /* Start the main menu */
+    runMenu(&field);
 
     return 0;
 }
