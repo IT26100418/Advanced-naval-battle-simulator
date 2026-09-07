abddd69875eb384c50a01e4607cac8748caafd03
KMG <it26100418@my.sliit.lk>
2026-09-07 17:31:55 +0530

    Main

--- main.c
+++ main.c
@@ -5,21 +5,45 @@
 #include "setup.h"
 #include "part1A.h"
 #include "part1B.h"
+#include "part1C.h"
+
 
 int main(void)
 {
     Battlefield field;
     Battlefield part1BField;
+    Battlefield part1CField;
 
     srand((unsigned int)time(NULL));
 
+    /*
+     * Create initial battlefield.
+     */
     initializeBattlefield(&field);
+
     displayBattlefield(&field);
 
+    /*
+     * Keep original battlefield
+     * for each part.
+     */
     part1BField = field;
+    part1CField = field;
 
+    /*
+     * Part 1-A
+     */
     runPart1A(&field);
+
+    /*
+     * Part 1-B
+     */
     runPart1B(&part1BField);
+
+    /*
+     * Part 1-C
+     */
+    runPart1C(&part1CField);
 
     return 0;
 }
