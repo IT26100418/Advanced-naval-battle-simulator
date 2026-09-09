23e6474e63e0236489ea189a4652378c2b22b7ce
KMG <it26100418@my.sliit.lk>
2026-09-09 23:25:19 +0530

    Types

--- types.h
+++ types.h
@@ -1,14 +1,11 @@
 #ifndef TYPES_H
 #define TYPES_H
-
 #define MAX_ESCORTS 100
 #define MAX_PATH_POINTS 100
-
 #define GRAVITY 9.81
 #define PI 3.141592653589793
 
-
-/* Escort ship types */
+//escort ship types
 typedef enum
 {
     EA,
@@ -18,8 +15,7 @@ typedef enum
     EE
 } EscortType;
 
-
-/* Battleship types */
+//battleship types
 typedef enum
 {
     U,
@@ -29,15 +25,14 @@ typedef enum
 } BattleshipType;
 
 
-/* Ship status */
+//ship status
 typedef enum
 {
     ALIVE,
     SUNK
 } ShipStatus;
 
-
-/* 2D position */
+//2D position
 typedef struct
 {
     double x;
@@ -45,21 +40,17 @@ typedef struct
 } Point;
 
 
-/* Battleship details */
+//battleship details
 typedef struct
 {
     BattleshipType type;
     char notation;
-
     Point position;
-
     double maxVelocity;
     double health;
     double gamma;
-
     int shotsFired;
     ShipStatus status;
-
     double lastShotVelocity;
     double lastShotAngle;
     double lastFlightTime;
@@ -67,37 +58,31 @@ typedef struct
 } Battleship;
 
 
-/* Escort ship details */
+//escort ship details
 typedef struct
 {
     int id;
     EscortType type;
-
     Point position;
-
     double minVelocity;
     double maxVelocity;
-
     double minAngle;
     double maxAngle;
-
     double impactPower;
     double gamma;
     double health;
-
     int shotsFired;
     ShipStatus status;
 
-    /* Part 2-B */
+    //part 2-B
     double firingInterval;
-
     double lastShotVelocity;
     double lastShotAngle;
     double lastFlightTime;
 
 } EscortShip;
 
-/* One path point */
+//one path point
 typedef struct
 {
     Point position;
@@ -105,48 +90,38 @@ typedef struct
 } PathPoint;
 
 
-/* Complete battlefield */
+//complete battlefield
 typedef struct
 {
     double size;
-
     Battleship battleship;
-
     EscortShip escorts[MAX_ESCORTS];
-
     int escortCount;
 
 } Battlefield;
 
-
-/* Result of one battle round */
+//result of one battle round
 typedef struct
 {
     int battleshipSunk;
     int killerEscortId;
     double killerTime;
-
     int hitCount;
     int hitIds[MAX_ESCORTS];
     double hitTimes[MAX_ESCORTS];
-
     double duration;
 
-    /* Used in Part 1-C */
+    //used in Part 1-C
     double cumulativeImpact;
 
 } BattleResult;
 
-
-/* Part 1-B settings */
+//part 1-B settings
 typedef struct
 {
     unsigned int randomSeed;
-
     int numberOfPathPoints;
-
     int gunJamIteration;
-
     double jamMinAngle;
 
 } SimulationSettings;
